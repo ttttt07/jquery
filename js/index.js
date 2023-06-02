3e8340b8f48fb4f05b480717618234d9c92e0172
ttttt07 <om.nom.omg.t@gmail.com>
2023-06-02 16:20:37 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$('#addHtml').html('<h1>New Element</h1>');
+$('.hide').hide(2000);
+$('section').first().hide();
+
+$('section').last().addClass('addClass').text('«Lumos Maxima»').css({'background': '#f66668','border-radius': '50% 0 50% 0','width': '200px','height': '120px','line-height': '80px','text-align': 'center'});
+
+
+
+
+
