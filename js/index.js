acc480f0abddbb6da38d9d6beef28f95463ff9be
ttttt07 <129382821+ttttt07@users.noreply.github.com>
2023-06-02 19:39:04 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $('#addHtml').html('<h1>New Element</h1>');
 $('.hide').hide(2000);
-$('section').first().hide();
+$('section').first().remove();
 
 $('section').last().addClass('addClass').text('«Lumos Maxima»').css({'background': '#f66668','border-radius': '50% 0 50% 0','width': '200px','height': '120px','line-height': '80px','text-align': 'center'});
 
